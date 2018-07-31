e3b476de975ee3fa878ff4198dd44fa32a42510d
José Garcia Vivas Miranda <vivasm@gmail.com>
2018-07-30 21:32:04 -0300

    Final

--- CuboLatino.m
+++ CuboLatino.m
@@ -1,10 +1,13 @@
 popSmin=10;
-popSmax=100;
-popTmin=10;
-popTmax=50;
-X = lhsdesign(45,2);
+popSmax=1000;
+popTmin=1;
+popTmax=100;
+pasMin=0.001;
+pasMax=0.1;
+X = lhsdesign(1000,3);
 X(:,1)=X(:,1)*(popSmax-popSmin) +popSmin;
 X(:,2)=X(:,2)*(popTmax-popTmin) +popTmin;
-scatter(X(:,1),X(:,2))
+X(:,3)=X(:,3)*(pasMax-pasMin) +pasMin;
+plot3(X(:,1),X(:,2),X(:,3),'o')
 
 
